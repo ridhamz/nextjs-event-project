5d51db4c32399bab9f36d6cf18944dab0a161507
ridha mezrigui <ridhamezrigui07@gmail.com>
2022-02-15 19:44:30 +0100

    add the layout and the navigation bar

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,27 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import { Fragment } from 'react';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 export default function EventDetailPage() {
-  return <div>EventDetailPage</div>;
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+  if (!event) return <h1>Invalid Id</h1>;
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 }
