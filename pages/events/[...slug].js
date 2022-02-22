bdc527ed3cee0cd961251fe4ef8427fcaed40d75
ridha mezrigui <ridhamezrigui07@gmail.com>
2022-02-22 20:00:51 +0100

    add filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,12 +1,17 @@
 import { useRouter } from 'next/router';
 import React from 'react';
+import EventList from '../../components/events/event-list';
+import { getFilteredEvents } from '../../dummy-data';
 
 export default function FilteredEventsPage() {
-    const router = useRouter()
-    const [year, month ] = router.query.slug
-     
-    if(!year || !month) return <p>Loading...</p>
-    const events =  12
-    
-  return <div>FilteredEventsPage</div>;
+  const router = useRouter();
+  const [year, month] = router.query.slug;
+
+  if (!year || !month) return <p>Loading...</p>;
+  const filteredEvents = getFilteredEvents({ year, month });
+
+  if (!filteredEvents || filteredEvents.length === 0)
+    return <p className="center">No events found for chosen filter!</p>;
+
+  return <EventList list={filteredEvents} />;
 }
